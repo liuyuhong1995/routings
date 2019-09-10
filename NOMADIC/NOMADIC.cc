2b69cc009f4c4a0b0483b3a70b21f3a4ff254cd9
pawn <247027417@qq.com>
2019-09-10 11:08:37 +0800

    add NOMADIC

---
+++ NOMADIC/NOMADIC.cc
@@ -0,0 +1,40 @@
+#include "ns3/aodv-module.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/wifi-module.h" 
+#include "ns3/applications-module.h"
+#include "ns3/netanim-module.h"
+#include "ns3/ns2-mobility-helper.h"
+
+
+using namespace ns3;
+int main(int argc, char **argv)
+{
+  uint32_t size=21;
+  double totalTime=100000;
+
+  NodeContainer nodes;
+  nodes.Create(size);
+
+  std::string traceFile = "scratch/NOMADIC/test.ns_movements";
+  Ns2MobilityHelper ns2 = Ns2MobilityHelper (traceFile);
+  ns2.Install ();
+  
+  // NodeContainer relay;
+  // relay.Create(1);
+  // MobilityHelper mobility;
+  // mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+  // mobility.Install(relay);
+  // relay.Get(0)->GetObject<ConstantPositionMobilityModel>()->SetPosition(Vector(300, 600, 0));
+
+  AnimationInterface anim ("/home/zlb/sim_temp/NOMADIC/output.xml");
+    
+  Simulator::Stop (Seconds(totalTime));
+  Simulator::Run ();
+  Simulator::Destroy ();
+  
+  std::cout<<"\n\n***** OUTPUT *****\n\n";
+  std::cout<<"Finished !"<<std::endl;
+}
