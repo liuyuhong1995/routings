a2be68f876bc5bd0288f26c1077a9d7b5c7f58d4
pawn <247027417@qq.com>
2019-10-15 20:23:56 +0800

    rocomar speed 10

--- ROCOMAR/ROCOMAR.cc
+++ ROCOMAR/ROCOMAR.cc
@@ -680,7 +680,7 @@ void createNode(){
 	// ns2.Install ();
 
 	if(!isEntity){
-		std::string traceFile = "scratch/"+mobilityModel+"/rocomar_speed5.ns_movements";
+		std::string traceFile = "scratch/"+mobilityModel+"/rocomar_speed10.ns_movements";
 		Ns2MobilityHelper ns2 = Ns2MobilityHelper (traceFile);
 		ns2.Install ();
 	}
